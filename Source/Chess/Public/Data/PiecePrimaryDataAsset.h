1ea5f86761bd53947ad8c15d8a7c28025d8e1a9b
KIMJIMIN <wlals3236@naver.com>
2026-06-17 22:49:21 +0900

    프로젝트 정리 및 리팩토링

---
+++ Source/Chess/Public/Data/PiecePrimaryDataAsset.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "PiecePrimaryDataAsset.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHESS_API UPiecePrimaryDataAsset : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+	
+};
